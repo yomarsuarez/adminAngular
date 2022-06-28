248729b3035f3e2a25184d5a542b5e65ae37ce3e
Yomar Suarez <yomaralveiro.suarez@gmail.com>
2022-06-28 11:29:50 -0500

    routes children

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule],
+  exports: [LoginComponent, RegisterComponent, RouterModule],
+})
+export class AuthModule {}
